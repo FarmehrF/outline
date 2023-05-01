38fa91d499918ed868d91ce230d768d62bcfc25d
Farmehr  Farsian <farmehr@Mac-mini.local>
2023-05-01 20:13:45 +0200

    out

---
+++ outline.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+curl -sS https://get.docker.com/ | sh
+systemctl start docker
+systemctl enable docker
+wget -qO- https://raw.githubusercontent.com/Jigsaw-Code/outline-server/master/src/server_manager/install_scripts/install_server.sh | bash
