8014f1101d4751bb7eb4bbf64e04e83d87571f83
FarmehrF <47416393+FarmehrF@users.noreply.github.com>
2023-05-12 09:57:13 +0200

    Update v2ray.sh

--- v2ray.sh
+++ v2ray.sh
@@ -3,6 +3,6 @@ apt install curl socat -y
 curl https://get.acme.sh | sh
 ~/.acme.sh/acme.sh --set-default-ca --server letsencrypt
 ~/.acme.sh/acme.sh --register-account -m ffarsiyan@gmail.com
-~/.acme.sh/acme.sh --issue -d $0 --standalone
-~/.acme.sh/acme.sh --installcert -d $0 --key-file /root/private.key --fullchain-file /root/cert.crt
+~/.acme.sh/acme.sh --issue -d $1 --standalone
+~/.acme.sh/acme.sh --installcert -d $1 --key-file /root/private.key --fullchain-file /root/cert.crt
 bash <(curl -Ls https://raw.githubusercontent.com/vaxilu/x-ui/master/install.sh)
